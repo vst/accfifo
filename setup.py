d2a92db80f2d5cdc8892ad4f8abddaa70a661ab8
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2015-04-30 12:58:09 +0800

    Getting ready to submit to PyPI

--- setup.py
+++ setup.py
@@ -1,28 +1,13 @@
 """
 This module provides the setup guidelines.
 """
-from setuptools import setup
-from setuptools import find_packages
-import os
-
-## The absolute directory path:
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-## The README file contents:
-README = open(os.path.join(HERE, "README.md")).read()
-
-## The LICENSE file contents:
-LICENSE = open(os.path.join(HERE, "LICENSE")).read()
-
-## Requirements for installation (non at the moment):
-REQUIRED_LIBRARIES = []
+from distutils.core import setup
 
 ## Setup now:
 setup(
     name="accfifo",
     version="0.1dev",
     description="A FIFO accounting calculator",
-    long_description=README,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -30,14 +15,9 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Topic :: Software Development :: Libraries",
     ],
-    keywords="accounting fifo",
+    keywords=["accounting", "fifo"],
     author="Vehbi Sinan Tunalioglu",
     author_email="vst@vsthost.com",
     url="https://github.com/vst/accfifo",
-    license=LICENSE,
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=REQUIRED_LIBRARIES,
-    dependency_links=[],
+    packages=["accfifo"],
 )
