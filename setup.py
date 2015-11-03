2aa5bfca7d9296366f5649ce542ff235c5ff5b44
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2015-11-03 09:53:04 +0800

    [chore] Development version bumped to 0.1.1dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 ## Setup now:
 setup(
     name="accfifo",
-    version="0.1",
+    version="0.1.1dev",
     description="A FIFO accounting calculator",
     classifiers=[
         "Development Status :: 3 - Alpha",
