5ddb6e77c0dfd13daab92ff2b152cbd6deb9c4c4
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2016-06-13 17:10:45 +0800

    [release] Version bumped to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 ## Setup now:
 setup(
     name="accfifo",
-    version="0.1.2dev",
+    version="0.1.2",
     description="A FIFO accounting calculator",
     classifiers=[
         "Development Status :: 3 - Alpha",
