5714def6ecf9b9655eaff8eec09228969029ad1f
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2022-08-01 10:00:54 +0800

    fix: adopt updated Pypi requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 """
 This module provides the setup guidelines.
 """
+import os
 from distutils.core import setup
 
 ## Setup now:
@@ -8,6 +9,10 @@ setup(
     name="accfifo",
     version="0.1.2",
     description="A FIFO accounting calculator",
+    long_description=open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.rst")
+    ).read(),
+    long_description_content_type="text/x-rst",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
